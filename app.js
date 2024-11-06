d1d375776bc0f4f7806f2ce7757e8e9da1a47d32
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2024-11-06 21:22:03 +0530

    Resolved issues in Search Country

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 function searchCountry() {
     let txtSearch = document.getElementById("txtSearch").value;
-
     fetch(`https://restcountries.com/v3.1/name/${txtSearch}`)
     .then(res => res.json())
     .then(data => {
         let body = "";
         data.forEach(element => {
             body += `
-                <div class="col-lg-8 mx-auto my-4 border-0">
-                    <div class="row g-0">
+                <div class="col-lg-8 mx-auto">
+                    <div class="row">
                         <div class="col-md-5 d-flex align-items-center justify-content-center">
                             <img src="${element.flags.png}" alt="${element.name.common} Flag" class="img-fluid rounded-3" style="max-height: 200px;">
                         </div>
-                        
                         <div class="col-md-7">
                             <div class="card-body">
                                 <h2 class="card-title text-primary fw-bold">${element.name.common}</h2>
                                 <p class="card-subtitle text-muted fst-italic">${element.name.official}</p>
-                                
                                 <table class="table table-bordered mt-3">
                                     <tbody>
                                         <tr><th>Capital</th><td>${element.capital}</td></tr>
@@ -30,9 +27,7 @@ function searchCountry() {
                                         <tr><th>Internet TLD</th><td>${element.tld}</td></tr>
                                         <tr><th>Driving Side</th><td>${element.car.side}</td></tr>
                                     </tbody>
-
                                 </table>
-                                
                                 <a href="${element.maps?.googleMaps || '#'}" target="_blank" class="btn btn-primary w-100 mt-3">View on Google Maps</a>
                             </div>
                         </div>
@@ -44,13 +39,8 @@ function searchCountry() {
     });
 }
 
-
-
-
-
 async function allCountries() {
-    console.log("All countries");
-
+    // console.log("All countries");
     let res = await fetch("https://restcountries.com/v3.1/all");
     let items = await res.json();
     let body = "";
@@ -67,7 +57,6 @@ async function allCountries() {
                 </div>
             </div>
         `;
-
     });
     document.getElementById("allCountriesRow").innerHTML = body;
 }
